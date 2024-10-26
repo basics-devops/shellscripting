86018c187aecf1310f9d02a7e8824db239fb36de
ganeshmoka <ganeshmoka1999@gmail.com>
2024-10-26 12:22:38 +0530

    shellscript

---
+++ 02-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+person1="Ramesh"
+person2="suresh"
+
+echo "$person1 : Hi ${person2} how are you"
+echo "$person2 : I am fine!"
---
+++ 03-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#assigning variables from arguments.
+
+person1=$1
+person2=$2
+
+echo "$person1 : Hi ${person2} how are you"
+echo "$person2 : I am fine!"
+
+#how to pass values to variables.?
+#sh 03-variables.sh "ramesh" "suresh"
+
