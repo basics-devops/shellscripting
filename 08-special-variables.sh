723b7d3d8ace6b3a7d5f7c31d7ccde44308d462c
ganeshmoka <ganeshmoka1999@gmail.com>
2024-10-29 07:33:32 +0530

    shellscript

---
+++ 08-special-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#$? --> exit status code 0 (success), 1 (failure)
+#$0  --> The current script name.
+#$# --> number of arguments passed.
+#$$ --> current pid.
+#$! --> last background command pid.
+#$@ --> all arguments passed.
